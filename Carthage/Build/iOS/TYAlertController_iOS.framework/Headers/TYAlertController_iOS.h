c7cb1df59181a2c0c8d0fd8579eb95e986f38885
stubbornnessness <1219721814yxj>
2016-10-10 22:51:49 +0800

    1.2版本开始支持carthage

---
+++ Carthage/Build/iOS/TYAlertController_iOS.framework/Headers/TYAlertController_iOS.h
@@ -0,0 +1,19 @@
+//
+//  TYAlertController_iOS.h
+//  TYAlertController_iOS
+//
+//  Created by yuanxiaojun on 2016/10/10.
+//  Copyright © 2016年 tanyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TYAlertController_iOS.
+FOUNDATION_EXPORT double TYAlertController_iOSVersionNumber;
+
+//! Project version string for TYAlertController_iOS.
+FOUNDATION_EXPORT const unsigned char TYAlertController_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TYAlertController_iOS/PublicHeader.h>
+
+
